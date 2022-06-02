eb7a80a0b582d3e412630a27d7c796ac381e3ad0
T. Herker <tim.herker@stud.hs-bochum.de>
2022-06-02 20:05:45 +0200

    Check all bbox points instead of only pMin and pMax when determining if sewer feature should be shown.
    This makes the check more accurate.
    Also remove epsg4326 bbox after conversion (not needed anymore)

--- src/js/webworkers/sewersWorker.js
+++ src/js/webworkers/sewersWorker.js
@@ -20,26 +20,24 @@ onmessage = function(e) {
  function calculateEntitiesToShow(planes, bboxes) {
     // Iterate tiles and check if they intersect with the view rectangle.
     let result = [];
-    console.log(planes);
     for(let bbox of bboxes) {
         let insideOrIntersectingFrustum = false;
-        for(let [idx, plane] of planes.entries()) {
-            // TODO create all 8 bbox points from pMin and pMax and check them. Checking pMin and pMax is not enough in some cases
-            let distanceP1; // pMin
-            let distanceP2; // pMax
-            distanceP1 =  calcDotProduct(plane.normal, bbox.epsg4978.pMin) + plane.distance;
-            distanceP2 = calcDotProduct(plane.normal, bbox.epsg4978.pMax) + plane.distance;
-            if(distanceP1 > 0 || distanceP2 > 0) {
-                // Both on same side as normal vector or intersecting plane
-                // Do nothing unless it is the last plane
-                if(idx === planes.length-1) {
-                    // Only true if all planes are okay, otherwise the loop ends early.
-                    insideOrIntersectingFrustum = true;
+        for(let [i, plane] of planes.entries()) {
+            let positiveDistanceFound = false;
+            for(let [j, point] of bbox.points.entries()) {
+                let distance = calcDotProduct(plane.normal, point) + plane.distance;
+                if(distance > 0) {
+                    positiveDistanceFound = true;
+                    break; // Point is in view
                 }
-            } else {
-                // Both points outside one frustum plane --> outside frustum
-                insideOrIntersectingFrustum = false;
-                break;
+            }
+            if(!positiveDistanceFound) {
+                break; // All points outside at least one plane
+            }
+
+            // Only reached if positiveDistanceFound is true for all planes
+            if(i == planes.length-1) {
+                insideOrIntersectingFrustum = true;
             }
         }
 
