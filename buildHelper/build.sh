028036c476b8be430faa7815785d003bedb7b8d5
T. Herker <49350695+t16h05008@users.noreply.github.com>
2022-11-20 13:28:15 +0100

    Create a shell script for the build process, which can be run by npm

---
+++ buildHelper/build.sh
@@ -0,0 +1,7 @@
+# build.sh
+#!/bin/bash
+
+# Build the dist folder, then use it to build the image and push it to DockerHub
+webpack --config webpack.config.prod.js && \
+docker image build --tag t16h05008/dt-herne-wanne-frontend . && \
+docker push t16h05008/dt-herne-wanne-frontend
